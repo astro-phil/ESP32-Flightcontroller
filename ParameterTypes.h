8dbc327565fda0e22653d7731360fbcea7cd96d1
Phil White <phil-micro@live.de>
2023-01-10 16:26:34 +0100

    Updated Controller Structure

--- ParameterTypes.h
+++ ParameterTypes.h
@@ -8,18 +8,24 @@
 #define PARAM_USER_THROTTLE_SENSE 11
 #define PARAM_USER_ROLLPITCH_SENSE 12
 #define PARAM_USER_YAW_SENSE 13
+#define PARAM_PID_Y_P 21
+#define PARAM_PID_Y_I 22
+#define PARAM_PID_Y_D 23
+#define PARAM_PID_Y_A 24
+#define PARAM_PID_YDOT_P 25
+#define PARAM_PID_YDOT_I 26
+#define PARAM_PID_YDOT_D 27
+#define PARAM_PID_YDOT_A 28
+#define PARAM_PID_PR_P 31
+#define PARAM_PID_PR_I 32
+#define PARAM_PID_PR_D 33
+#define PARAM_PID_PR_A 34
+#define PARAM_PID_PRDOT_P 35
+#define PARAM_PID_PRDOT_I 36
+#define PARAM_PID_PRDOT_D 37
+#define PARAM_PID_PRDOT_A 38
 
-#define PARAM_PID_YAW_P 21
-#define PARAM_PID_YAW_I 22
-#define PARAM_PID_YAW_D 23
-#define PARAM_PID_YAW_A 24
-
-#define PARAM_PID_PITCH_ROLL_P 25
-#define PARAM_PID_PITCH_ROLL_I 26
-#define PARAM_PID_PITCH_ROLL_D 27
-#define PARAM_PID_PITCH_ROLL_A 28
-
-#define NUM_PARAMETERS_USED 13
+#define NUM_PARAMETERS_USED 21
 
 typedef struct {
   float parameters[NUM_PARAMETERS_RANGE];
@@ -27,23 +33,31 @@ typedef struct {
   uint8_t usedParameters;
 } ParameterSet;
 
-static uint8_t usedIndex[NUM_PARAMETERS_USED] = { 1, 2, 11, 12, 13, 21, 22, 23, 24, 25, 26, 27, 28 };
+static uint8_t usedIndex[NUM_PARAMETERS_USED] = {1, 2, 11, 12, 13, 21, 22, 23, 24, 25, 26, 27, 28,31,32,33,34,35,36,37,38};
 
 static void setParameterDefaults(ParameterSet* _paramSet, uint8_t* _usedIndex) {
   _paramSet->usedParameters = NUM_PARAMETERS_USED;
   _paramSet->parameterIndex = _usedIndex;
   _paramSet->parameters[PARAM_SYSTEM_UPDATECYCLE] = 3;
   _paramSet->parameters[PARAM_USER_THROTTLE_SENSE] = 0.5;
-  _paramSet->parameters[PARAM_USER_ROLLPITCH_SENSE] = 100;
-  _paramSet->parameters[PARAM_USER_YAW_SENSE] = 100;
-  _paramSet->parameters[PARAM_PID_YAW_P] = 0.01;
-  _paramSet->parameters[PARAM_PID_YAW_I] = 0.0;
-  _paramSet->parameters[PARAM_PID_YAW_D] = 0.05;
-  _paramSet->parameters[PARAM_PID_YAW_A] = 0.05;
-  _paramSet->parameters[PARAM_PID_PITCH_ROLL_P] = 0.001;
-  _paramSet->parameters[PARAM_PID_PITCH_ROLL_I] = 0.0;
-  _paramSet->parameters[PARAM_PID_PITCH_ROLL_D] = 0.005;
-  _paramSet->parameters[PARAM_PID_PITCH_ROLL_A] = 0.05;
+  _paramSet->parameters[PARAM_USER_ROLLPITCH_SENSE] = 5;
+  _paramSet->parameters[PARAM_USER_YAW_SENSE] = 5;
+  _paramSet->parameters[PARAM_PID_Y_P] = 0.0;
+  _paramSet->parameters[PARAM_PID_Y_I] = 0.0;
+  _paramSet->parameters[PARAM_PID_Y_D] = 0.0;
+  _paramSet->parameters[PARAM_PID_Y_A] = 0.05;
+  _paramSet->parameters[PARAM_PID_YDOT_P] = 0.1;
+  _paramSet->parameters[PARAM_PID_YDOT_I] = 0.0;
+  _paramSet->parameters[PARAM_PID_YDOT_D] = 0.0;
+  _paramSet->parameters[PARAM_PID_YDOT_A] = 0.05;
+  _paramSet->parameters[PARAM_PID_PR_P] = 0.0;
+  _paramSet->parameters[PARAM_PID_PR_I] = 0.0;
+  _paramSet->parameters[PARAM_PID_PR_D] = 0.0;
+  _paramSet->parameters[PARAM_PID_PR_A] = 0.05;
+  _paramSet->parameters[PARAM_PID_PRDOT_P] = 0.01;
+  _paramSet->parameters[PARAM_PID_PRDOT_I] = 0.0;
+  _paramSet->parameters[PARAM_PID_PRDOT_D] = 0.0;
+  _paramSet->parameters[PARAM_PID_PRDOT_A] = 0.05;
 }
 
 #endif
--- sketch_flightcontroller.ino
+++ sketch_flightcontroller.ino
@@ -34,9 +34,13 @@ InputState inputStt;
 Integrator yawI;
 Integrator pitchI;
 Integrator rollI;
-PID yawPID(PARAM_PID_YAW_P, PARAM_PID_YAW_I, PARAM_PID_YAW_D, PARAM_PID_YAW_A);
-PID pitchPID(PARAM_PID_PITCH_ROLL_P, PARAM_PID_PITCH_ROLL_I, PARAM_PID_PITCH_ROLL_D, PARAM_PID_PITCH_ROLL_A);
-PID rollPID(PARAM_PID_PITCH_ROLL_P, PARAM_PID_PITCH_ROLL_I, PARAM_PID_PITCH_ROLL_D, PARAM_PID_PITCH_ROLL_A);
+
+PID yawPID(PARAM_PID_Y_P, PARAM_PID_Y_I, PARAM_PID_Y_D, PARAM_PID_Y_A);
+PID yawDotPID(PARAM_PID_YDOT_P, PARAM_PID_YDOT_I, PARAM_PID_YDOT_D, PARAM_PID_YDOT_A);
+PID pitchPID(PARAM_PID_PR_P, PARAM_PID_PR_I, PARAM_PID_PR_D, PARAM_PID_PR_A);
+PID pitchDotPID(PARAM_PID_PRDOT_P, PARAM_PID_PRDOT_I, PARAM_PID_PRDOT_D, PARAM_PID_PRDOT_A);
+PID rollPID(PARAM_PID_PR_P, PARAM_PID_PR_I, PARAM_PID_PR_D, PARAM_PID_PR_A);
+PID rollDotPID(PARAM_PID_PRDOT_P, PARAM_PID_PRDOT_I, PARAM_PID_PRDOT_D, PARAM_PID_PRDOT_A);
 
 // defifne two tasks for Blink & AnalogRead
 void WifiLoop(void *pvParameters);
@@ -52,6 +56,9 @@ void resetControl(){
   yawPID.reset();
   pitchPID.reset();
   rollPID.reset();
+  yawDotPID.reset();
+  pitchDotPID.reset();
+  rollDotPID.reset();
 }
 
 
@@ -72,6 +79,9 @@ void setup() {
   yawPID.setPointer(&paramSet, &systemStt);
   pitchPID.setPointer(&paramSet, &systemStt);
   rollPID.setPointer(&paramSet, &systemStt);
+  yawDotPID.setPointer(&paramSet, &systemStt);
+  pitchDotPID.setPointer(&paramSet, &systemStt);
+  rollDotPID.setPointer(&paramSet, &systemStt);
   sensPool.setPointer(&sensorStt, &systemStt);
   sensPool.setup();
   wifi.begin();
@@ -109,6 +119,7 @@ void loop() {
       inputStt.arm = motorStt.armed;
       resetControl();
     }
+
     motorStt.Control.w = control.throttle / 32000.0 * paramSet.parameters[PARAM_USER_THROTTLE_SENSE];
     inputStt.AngularVelocity.x = control.pitch / 32000.0 * paramSet.parameters[PARAM_USER_ROLLPITCH_SENSE];
     inputStt.AngularVelocity.y = control.roll / 32000.0 * paramSet.parameters[PARAM_USER_ROLLPITCH_SENSE];
@@ -116,9 +127,10 @@ void loop() {
     inputStt.Angle.x = pitchI.step(inputStt.AngularVelocity.x);
     inputStt.Angle.y = rollI.step(inputStt.AngularVelocity.y);
     inputStt.Angle.z = yawI.step(inputStt.AngularVelocity.z);
-    motorStt.Control.x = pitchPID.step(sensorStt.Attitude.x, sensorStt.AngularVelocity.x, inputStt.Angle.x, inputStt.AngularVelocity.x);
-    motorStt.Control.y = rollPID.step(sensorStt.Attitude.y, sensorStt.AngularVelocity.y, inputStt.Angle.y, inputStt.AngularVelocity.y);
-    motorStt.Control.z = yawPID.step(sensorStt.Attitude.z, sensorStt.AngularVelocity.z, inputStt.Angle.z, inputStt.AngularVelocity.z);
+
+    motorStt.Control.z = yawPID.step(sensorStt.Attitude.z, inputStt.Angle.z) + yawDotPID.step(sensorStt.AngularVelocity.z,inputStt.AngularVelocity.z);
+    motorStt.Control.x = pitchPID.step(sensorStt.Attitude.x, inputStt.Angle.x) + pitchDotPID.step(sensorStt.AngularVelocity.x,inputStt.AngularVelocity.x);
+    motorStt.Control.y = rollPID.step(sensorStt.Attitude.y, inputStt.Angle.y) + rollDotPID.step(sensorStt.AngularVelocity.y,inputStt.AngularVelocity.y);
     //Serial.printf("pitch %f:%f ; roll %f:%f ; yaw %f:%f \n", sensorStt.AngularVelocity.x, sensorStt.Attitude.x,  sensorStt.AngularVelocity.y, sensorStt.Attitude.y,  sensorStt.AngularVelocity.z, sensorStt.Attitude.z);
     //Serial.printf("pitch %f:%f ; roll %f:%f ; yaw %f:%f \n", inputStt.AngularVelocity.x, inputStt.Angle.x,  inputStt.AngularVelocity.y, inputStt.Angle.y,  inputStt.AngularVelocity.z, inputStt.Angle.z);
 
